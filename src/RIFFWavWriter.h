36c7931df564b04845a3a53db3d09502001c5ea8
Paul Walker <paul@pwjw.com>
2024-12-01 17:56:54 -0500

    Write a test wav file

---
+++ src/RIFFWavWriter.h
@@ -0,0 +1,193 @@
+/*
+ * SampleCreator
+ *
+ * An experimental idea based on a preliminary convo. Probably best to come back later.
+ *
+ * Copyright Paul Walker 2024
+ *
+ * Released under the MIT License. See `LICENSE.md` for details
+ */
+
+#ifndef SRC_RIFFWAVWRITER_HPP
+#define SRC_RIFFWAVWRITER_HPP
+
+#include <filesystem>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+
+namespace baconpaul::fm
+{
+/*
+ * A very simple RIFF Wav Writer which *just* writes F32 stereo wav files
+ * with an inst block.
+ */
+struct RIFFWavWriter
+{
+    std::filesystem::path outPath{};
+    FILE *outf{nullptr};
+    size_t elementsWritten{0};
+    size_t fileSizeLocation{0};
+    size_t dataSizeLocation{0};
+    size_t dataLen{0};
+
+    uint16_t nChannels{2};
+
+    std::string errMsg{};
+
+    RIFFWavWriter() {}
+
+    RIFFWavWriter(const std::filesystem::path &p, uint16_t chan) : outPath(p), nChannels(chan) {}
+    ~RIFFWavWriter()
+    {
+        if (!closeFile())
+        {
+            // Unhandleable error here. Throwing is bad. Reporting is useless.
+        }
+    }
+
+    void writeRIFFHeader()
+    {
+        pushc4('R', 'I', 'F', 'F');
+        fileSizeLocation = elementsWritten;
+        pushi32(0);
+        pushc4('W', 'A', 'V', 'E');
+    }
+
+    void writeFMTChunk(int32_t samplerate)
+    {
+        pushc4('f', 'm', 't', ' ');
+        pushi32(16);
+        pushi16(3);         // IEEE float
+        pushi16(nChannels); // channels
+        pushi32(samplerate);
+        pushi32(samplerate * nChannels * 4); // channels * bytes * samplerate
+        pushi16(nChannels * 4);              // align on pair of 4 byte samples
+        pushi16(8 * 4);                      // bits per sample
+    }
+
+    void writeINSTChunk(char keyroot, char keylow, char keyhigh, char vellow, char velhigh)
+    {
+        pushc4('i', 'n', 's', 't');
+        pushi32(8);
+        pushi8(keyroot);
+        pushi8(0);
+        pushi8(127);
+        pushi8(keylow);
+        pushi8(keyhigh);
+        pushi8(vellow);
+        pushi8(velhigh);
+        pushi8(0);
+    }
+
+    void startDataChunk()
+    {
+        pushc4('d', 'a', 't', 'a');
+        dataSizeLocation = elementsWritten;
+        pushi32(0);
+    }
+
+    void pushSamples(float d[2])
+    {
+        if (outf)
+        {
+            elementsWritten += fwrite(d, 1, nChannels * sizeof(float), outf);
+            dataLen += nChannels * sizeof(float);
+        }
+    }
+
+    void pushInterleavedBlock(float *d, size_t nSamples)
+    {
+        if (outf)
+        {
+            elementsWritten += fwrite(d, 1, nSamples * sizeof(float), outf);
+            dataLen += nSamples * sizeof(float);
+        }
+    }
+
+    void pushc4(char a, char b, char c, char d)
+    {
+        char f[4]{a, b, c, d};
+        pushc4(f);
+    }
+    void pushc4(char f[4])
+    {
+        if (outf)
+            elementsWritten += fwrite(f, sizeof(char), 4, outf);
+    }
+
+    void pushi32(int32_t i)
+    {
+        if (outf)
+            elementsWritten += std::fwrite(&i, sizeof(char), sizeof(uint32_t), outf);
+    }
+
+    void pushi16(int16_t i)
+    {
+        if (outf)
+            elementsWritten += fwrite(&i, sizeof(char), sizeof(uint16_t), outf);
+    }
+
+    void pushi8(char i)
+    {
+        if (outf)
+            elementsWritten += std::fwrite(&i, 1, 1, outf);
+    }
+    [[nodiscard]] bool openFile()
+    {
+        elementsWritten = 0;
+        dataLen = 0;
+        dataSizeLocation = 0;
+        fileSizeLocation = 0;
+
+        try
+        {
+            outf = fopen(outPath.u8string().c_str(), "wb");
+            if (!outf)
+            {
+                errMsg = "Unable to open '" + outPath.u8string() + "' for writing";
+                return false;
+            }
+        }
+        catch (const std::filesystem::filesystem_error &e)
+        {
+            outf = nullptr;
+            errMsg = e.what();
+            return false;
+        }
+        return true;
+    }
+
+    bool isOpen() { return outf != nullptr; }
+    [[nodiscard]] bool closeFile()
+    {
+        if (outf)
+        {
+            int res;
+            res = std::fseek(outf, fileSizeLocation, SEEK_SET);
+            if (res)
+            {
+                std::cout << "SEEK ZERO ERROR" << std::endl;
+                return false;
+            }
+            int32_t chunklen = elementsWritten - 8; // minus riff and size
+            fwrite(&chunklen, sizeof(uint32_t), 1, outf);
+
+            res = std::fseek(outf, dataSizeLocation, SEEK_SET);
+            if (res)
+            {
+                return false;
+                std::cout << "SEEK ONE ERROR" << std::endl;
+            }
+            fwrite(&dataLen, sizeof(uint32_t), 1, outf);
+            std::fclose(outf);
+            outf = nullptr;
+        }
+        return true;
+    }
+
+    [[nodiscard]] size_t getSampleCount() const { return dataLen / (nChannels * sizeof(float)); }
+};
+} // namespace baconpaul::samplecreator::riffwav
+#endif // SAMPLECREATOR_RIFFWAVWRITER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <cstdint>
 
 #include "sintable.h"
+#include "RIFFWavWriter.h"
+#include <cassert>
 
 int main(int, char **)
 {
@@ -16,19 +18,32 @@ int main(int, char **)
     auto dph = st.dPhase(fr, sr);
     uint32_t ph = 0;
 
-    for (int i = 0; i < 500; ++i)
+    baconpaul::fm::RIFFWavWriter writer("test.wav", 2);
+    writer.openFile();
+    assert(writer.isOpen());
+    writer.writeRIFFHeader();
+    writer.writeFMTChunk(sr);
+    writer.startDataChunk();
+    float smp[2];
+
+    for (int i = 0; i < 500000; ++i)
     {
+        if ((i+1) % 50000 == 0)
+        {
+            std::cout << pow(2.0, 1.0/12.0) << std::endl;;
+            fr *= pow(2.0, 1.0 / 12.0);
+            dph = st.dPhase(fr, sr);
+        }
+
         auto res = st.at(ph);
         auto ans = sin(1.0 * ph / (1 << 26) * 2.0 * M_PI);
-        if (i % 10 == 0)
-        {
-            std::cout << "i=" << i
-                      << " sf=" << res << " sv=" << ans << " diff=" << 1e5 * (ans - res) << " x1e-5"
-            << std::endl;
-        }
+        smp[0] = res;
+        smp[1] = res;
+        writer.pushSamples(smp);
 
         ph += dph;
     }
 
+    writer.closeFile();
     std::cout << "hw" << std::endl;
 }
