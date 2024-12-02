e08b05a2ea9eabf5fb7bf20bea57ba2cccced3e4
Paul Walker <paul@pwjw.com>
2024-12-01 20:35:47 -0500

    show some basic FM working

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,13 @@ int main(int, char **)
     uint32_t phase;
 
     auto sr = 96000.0;
-    auto fr = 440.0;
+    auto fr = 55.0;
 
+    auto frac = 3.7;
     auto dph = st.dPhase(fr, sr);
-    uint32_t ph = 0;
+    auto dph2 = st.dPhase(fr * frac, sr);
+    uint32_t ph = 4 << 27;
+    uint32_t cph = 4 << 27;
 
     baconpaul::fm::RIFFWavWriter writer("test.wav", 2);
     writer.openFile();
@@ -26,17 +29,27 @@ int main(int, char **)
     writer.startDataChunk();
     float smp[2];
 
-    for (int i = 0; i < 500000; ++i)
+    auto tri = [](auto f)
+    {
+        if (f < 0.5)
+            return f * 2;
+        else
+            return (1-f) * 2;
+    };
+
+    for (int i = 0; i < 800000; ++i)
     {
         if ((i+1) % 50000 == 0)
         {
-            std::cout << pow(2.0, 1.0/12.0) << std::endl;;
-            fr *= pow(2.0, 1.0 / 12.0);
+            fr *= pow(2.0, 4.0 / 12.0);
             dph = st.dPhase(fr, sr);
+            dph2 = st.dPhase(fr * frac, sr);
         }
 
-        auto res = st.at(ph);
-        auto ans = sin(1.0 * ph / (1 << 26) * 2.0 * M_PI);
+        auto p1 = st.at(cph) * tri(1.f * (i % 50000) / 50000);
+        cph += dph2;
+
+        auto res = st.at(ph + (int32_t) (p1 * (1<<26)));
         smp[0] = res;
         smp[1] = res;
         writer.pushSamples(smp);
