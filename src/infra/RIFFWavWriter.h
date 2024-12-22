aaa978f1daa39cd848bb2fbd332d69929969d37e
Paul Walker <paul@pwjw.com>
2024-12-22 12:34:32 -0500

    Change name from "Six Sines A-Sinnin" to just "Six Sines"

--- src/infra/RIFFWavWriter.h
+++ src/infra/RIFFWavWriter.h
@@ -1,14 +1,16 @@
 /*
- * Six Sines A Sinnin'
+ * Six Sines
  *
- * A mess, with audio rate modulation.
+ * A synth with audio rate modulation.
  *
  * Copyright 2024, Paul Walker and Various authors, as described in the github
  * transaction log.
  *
  * This source repo is released under the MIT license, but has
- * GPL3 dependencies, as such the combined work can also be
- * released under GPL3. You know the drill.
+ * GPL3 dependencies, as such the combined work will be
+ * released under GPL3.
+ *
+ * The source code and license are at https://github.com/baconpaul/six-sines
  */
 
 #ifndef BACONPAUL_SIX_SINES_INFRA_RIFFWAVWRITER_H
