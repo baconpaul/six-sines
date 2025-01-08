879b568221de771b38e67369b57d5f3d03a4fe64
baconpaul <paul@pwjw.com>
2025-01-08 14:20:46 -0500

    Not for merging

--- src/clap/six-sines-clap-entry.cpp
+++ src/clap/six-sines-clap-entry.cpp
@@ -15,6 +15,14 @@
 
 #include <clap/clap.h>
 #include "six-sines-clap-entry-impl.h"
+#if WIN32
+#include <windows.h>
+#endif
+
+#include <iostream>
+#define LG std::cout << __FILE__ << ":" << __LINE__ << " "
+
+#include "../../libs/clap-libs/clap-wrapper/src/detail/shared/sha1.h"
 
 extern "C"
 {
@@ -24,10 +32,45 @@ extern "C"
 #pragma GCC diagnostic ignored "-Wattributes"
 #endif
 
+    bool custom_clap_init(const char *p) {
+#if WIN32
+#define WIN_DEBUG_CONSOLE_ACTIVE 1
+#if WIN_DEBUG_CONSOLE_ACTIVE
+        static FILE *confp{nullptr};
+        AllocConsole();
+        freopen_s(&confp, "CONOUT$", "w", stdout);
+        HANDLE hConOut =
+            CreateFile(L"CONOUT$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE,
+                       NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        SetStdHandle(STD_OUTPUT_HANDLE, hConOut);
+        std::cout.clear();
+        std::wcout.clear();
+        printf("Hello Console\n");
+        std::cout << "Heya" << std::endl;
+#endif
+#endif
+
+        LG << "Custom Clap Init " << p << std::endl;
+
+        std::string id{"org.thisthat.plugin.name"};
+        auto g = Crypto::create_sha1_guid_from_name(id.c_str(), id.size());
+        char c[2048];
+        memcpy(c, &g, sizeof(Crypto::uuid_object));
+        LG << "UUID : " << std::hex;
+        for (int i=0; i<sizeof(Crypto::uuid_object); ++i)
+        {
+            std::cout << (int)c[i];
+        }
+        std::cout << std::dec << std::endl;
+
+        return true;
+    }
+
     // clang-format off
     const CLAP_EXPORT struct clap_plugin_entry clap_entry = {
         CLAP_VERSION,
-        baconpaul::six_sines::clap_init,
+        // baconpaul::six_sines::clap_init,
+custom_clap_init,
         baconpaul::six_sines::clap_deinit,
         baconpaul::six_sines::get_factory
     };
