b2e347b0c753f8f91e11c01d0c6cf97b4c8ffb14
Paul <baconpaul@users.noreply.github.com>
2025-02-16 16:24:02 -0500

    An option to reposition sources and matrix (#259)
    It's signal-path-visualization vs mouse-motion-distance
    
    Closes #258

--- src/ui/ui-defaults.h
+++ src/ui/ui-defaults.h
@@ -26,6 +26,7 @@ enum Defaults
     useLightSkin,
     zoomLevel,
     useSoftwareRenderer, // only used on windows
+    flipSourceAndMatrix,
     numDefaults
 };
 
@@ -39,6 +40,8 @@ inline std::string defaultName(Defaults d)
         return "zoomLevel";
     case useSoftwareRenderer:
         return "useSoftwareRenderer";
+    case flipSourceAndMatrix:
+        return "flipSourceAndMatrix";
     case numDefaults:
     {
         SXSNLOG("Software Error - defaults found");
